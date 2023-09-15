ba1c8e489c302e57533f38fc81702bc1c3056b01
buhairong <22858111@qq.com>
2023-09-15 09:39:44 +0800

    CSS初始化

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { App } from './App'
 import { router } from './router'
+import './style/index.scss'
 
 const app = createApp(App)
 app.use(router)
--- src/views/welcome/Welcome.tsx
+++ src/views/welcome/Welcome.tsx
@@ -1,10 +1,14 @@
 import { defineComponent } from 'vue'
 import style from './welcome.module.scss'
+import logo from '../../assets/icons/mangosteen.svg'
 
 export const Welcome = defineComponent({
   setup() {
     return () => <div class={style.wrapper}>
-      <header>logo</header>
+      <header>
+        <img src={logo} />
+        <h1>玄睻记账</h1>
+      </header>
       <main><router-view></router-view></main>
       <footer>footer</footer>
     </div>

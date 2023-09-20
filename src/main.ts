e39af1e3aa6ec840b93bf632b8b0199ae3ba5306
buhairong <22858111@qq.com>
2023-09-20 14:20:26 +0800

    14

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { App } from './App'
 import { router } from './router'
 import './style/index.scss'
+import '@svgstore'
 
 const app = createApp(App)
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { svgstore } from "./src/vite_plugins/svgstore"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,8 @@ export default defineConfig({
     vue(),
     vueJsx({
       transformOn: true,
-      mergeProps: true
-    })
+      mergeProps: true,
+    }),
+    svgstore(),
   ],
-})
+});

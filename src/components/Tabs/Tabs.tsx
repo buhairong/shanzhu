79ef73d7b9f6a83e222bc7c6b9a9cfb3dfc9f9d5
buhairong <buhairong@51cheyaoshi.com>
2023-10-09 17:51:01 +0800

    18

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,9 @@
+import { defineComponent } from 'vue'
+
+export const Tabs = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      <div>tabs</div>
+    )
+  }
+})
--- src/views/items/Create.tsx
+++ src/views/items/Create.tsx
@@ -1,9 +1,34 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { Icon } from '../../components/Icon/Icon'
+import { MainLayout } from '../../components/Layouts/MainLayout'
+import { Tab } from '../../components/Tabs/Tab'
+import { Tabs } from '../../components/Tabs/Tabs'
 
 export const Create = defineComponent({
   setup: (props, context) => {
+    const refKind = ref('支出')
+    const onUpdateSelected = (name: string) => {
+      refKind.value = name
+    }
     return () => (
-      <div>hi</div>
+      <MainLayout>
+        {
+          {
+            title: () => '记一笔',
+            icon: () => <Icon name="left" />,
+            default: () => <>
+              <Tabs selected={refKind.value} onUpdateSelected={onUpdateSelected}>
+                <Tab name="支出">
+                  icon 列表
+                </Tab>
+                <Tab name="收入">
+                  icon 列表2
+                </Tab>
+              </Tabs>
+            </>
+          }
+        }
+      </MainLayout>
     )
   }
 })
